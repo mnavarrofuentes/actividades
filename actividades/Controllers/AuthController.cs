b77d563c537233bc4495eaa614c0a420157196db
Milton Navarro <mnavarro@sigel.gt>
2024-05-13 22:26:30 -0600

    contrato de auth

---
+++ actividades/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using actividades.Contracts.auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace actividades.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost("login")]
+        public ActionResult Login(LoginRequest request)
+        {
+            var token = GenerateToken();
+            return Ok(new { token });
+        }
+
+        [HttpPost("logout")]
+        public ActionResult Logout()
+        {
+            return Ok();
+        }
+
+        private string GenerateToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
