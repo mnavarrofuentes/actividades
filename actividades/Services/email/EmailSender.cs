8e64a5f77015b6fc684b98d090b4507ca9694f81
Milton Navarro <mnavarro@sigel.gt>
2024-06-03 20:23:18 -0600

    correo

---
+++ actividades/Services/email/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mail;
+using actividades.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+public class EmailSender
+{
+    public async Task SendTaskNotificationEmail(Tarea tarea, string userEmail)
+    {
+        Console.WriteLine("correo"+userEmail);
+        try
+        {
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Actividades", "noreplyactividades.com"));
+            message.To.Add(new MailboxAddress(userEmail, userEmail));
+            message.Subject = "Tarea próxima a vencer";
+
+            var builder = new BodyBuilder();
+            builder.TextBody = $"La tarea '{tarea.Nombre}' está próxima a vencer el {tarea.FechaLimite?.ToString("dd/MM/yyyy")}.";
+            builder.HtmlBody = $"La tarea '{tarea.Nombre}' está próxima a vencer el {tarea.FechaLimite?.ToString("dd/MM/yyyy")}.";
+            message.Body = builder.ToMessageBody();
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false); // Use your SMTP server and port
+                client.Authenticate("miltonnavas7@gmail.com", "pzvk adcl exni owpq"); // Use your SMTP username and password
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("errorrrrr");
+            // Manejo de errores
+        }
+    }
+}
---
+++ actividades/Services/email/TaskReminderJob.cs
@@ -0,0 +1,39 @@
+﻿using Quartz;
+using actividades.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class TaskReminderJob : IJob
+{
+    private readonly control_tareasContext dbContext;
+    private readonly EmailSender emailSender;
+
+    public TaskReminderJob(control_tareasContext dbContext, EmailSender emailSender)
+    {
+        this.dbContext = dbContext;
+        this.emailSender = emailSender;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine("Ejecutando tarea de recordatorio...");
+        // Obtiene la fecha de hoy como DateOnly
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        Console.WriteLine("today" + today);
+
+        // Lógica para consultar las tareas próximas a vencer y enviar correos electrónicos
+        var tasks = dbContext.Tareas.Where(t => t.FechaLimite == today).ToList();
+
+        Console.WriteLine("taks" + tasks.Count);
+
+        foreach (var task in tasks)
+        {
+            Console.WriteLine("mira"+task.Id);
+            var asignacionUsuario = await dbContext.AsignacionUsuarios.FirstOrDefaultAsync(au => au.TareaId == task.Id);
+            var usuarioAsignado = asignacionUsuario != null ? await dbContext.Usuarios.FindAsync(asignacionUsuario.UsuarioId) : null;
+            var correoUsuarioAsignado = usuarioAsignado?.Correo;
+            // Aquí envías el correo electrónico de notificación para cada tarea
+            await emailSender.SendTaskNotificationEmail(task, correoUsuarioAsignado);
+        }
+    }
+}
